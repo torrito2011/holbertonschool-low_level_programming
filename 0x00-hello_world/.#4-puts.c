3a74351f45fe522a848d956deab7198955a43371
torres <torrito2011>
2021-02-04 19:11:29 +0000

    hello

---
+++ 0x00-hello_world/.#4-puts.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.8152:1612365019
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - This uses program prints exactly the string
- * "Programming is like building a multilingual puzzle
- * followed by a new line.
+ * followed by a new line
+ *
  * Return: always 0 (Success)
  */
 int main(void)
