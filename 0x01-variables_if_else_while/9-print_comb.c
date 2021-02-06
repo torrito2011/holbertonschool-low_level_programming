a8a31dc835c4fe6ef70fccbc80c3e64f8ee129aa
torres <torrito2011>
2021-02-06 06:41:19 +0000

    make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - This program prints all possible combinations of
-* single-digit numbers.
-* Numbers are separated by a comma, printed in ascending order,
-* and is printed with putchar only.
-* Return: Always 0 (Success)
-*/
- int main(void)
+ * main - This program prints all possible combinations of
+ * single-digit numbers.
+ * Numbers are separated by a comma, printed in ascending order,
+ * and is printed with putchar only.
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int i;
 
